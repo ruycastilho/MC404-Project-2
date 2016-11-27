84349428c67e8076c362b4042e5bfd0bd4a2b465
Ruy Castilho <ruy.castilho26@gmail.com>
2016-11-26 22:46:28 -0200

    .c files recreated. Makefile fixed.

---
+++ segue-parede.c
@@ -0,0 +1,17 @@
+// MC404 - Trabalho 2 - Subcamada LoCo - Segue-Parede
+// Ruy Castilho Barrichelo - RA 177012
+
+
+// INCLUDES
+#include "uoli_api.h"
+
+// DEFINES
+
+
+
+// CODE
+
+
+void segueParede();
+void buscaParede();
+
